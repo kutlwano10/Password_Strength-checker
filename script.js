253c678097d25aeeec9575f54e7e6f7021d18fc0
kutlwano Ramotebele <111123714+kutlwano10@users.noreply.github.com>
2024-03-14 13:01:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-function passwordInput(password) {
-    if (/[a-zA-Z]\d+[!@#$ %^&*(_ +=}: "?></.~`]/.test(password)) {
-        return "Strong 🟢";
-
-    } else if (/[a-zA-Z]\d+/.test(password)) {
-        return "Medium 🟡";
-    } else {
-        return "Weak 🔴"
-    }
-}
-
-
-
 
 
 // Function to check the strength of the password
@@ -20,5 +7,5 @@ function checkPassword() {
     // Your password strength checking logic goes here
 
     // Example: Display the result in the result paragraph
-    document.getElementById("result").textContent = passwordInput(password); // Example result
+    document.getElementById("result").textContent = ; // Example result
 }
