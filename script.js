3b1a65aebe1b7e0e323b076f093aca62b6f3de2e
TshireletsoMogwere <tmogwere09@gmail.com>
2024-03-14 14:39:46 +0200

    added password validator

--- script.js
+++ script.js
@@ -3,9 +3,22 @@
 // Function to check the strength of the password
 function checkPassword() {
     var password = document.getElementById("password").value;
+   
+    //insert password example
 
+    
     // Your password strength checking logic goes here
+    //check using pattern length
+    if (password.length < 5) {
+        result = "Weak";
+    } else if (password.length < 10 ) {
+        result = "Medium";
+    } else {
+        result = "Strong";
+    }
+
+
 
     // Example: Display the result in the result paragraph
-    document.getElementById("result").textContent = ; // Example result
+    document.getElementById("result").innerHTML = result; // Example result
 }
